569321d11086cd42d672c5114a4403e1dc0151f8
Mikan <meteorcheung7@gmail.com>
2021-11-22 14:16:08 -0500

    #Changed the panel size and added try-catch block to handle number format exception  in MenubarListener

--- src/controller/MenubarListener.java
+++ src/controller/MenubarListener.java
@@ -2,45 +2,44 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+
 import model.FeetConversionArea;
 import model.MeterConversionArea;
 
 import view.ConverterPanel;
 
 public class MenubarListener implements ActionListener {
-//	private FeetConversionArea feetArea;
+    //	private FeetConversionArea feetArea;
 //	private MeterConversionArea meterArea;
-	private ConverterPanel panel;
+    private ConverterPanel panel;
 
-	public MenubarListener(ConverterPanel converterPanel) {
-		// TODO Auto-generated constructor stub
-		this.panel = converterPanel;
-	}
+    public MenubarListener(ConverterPanel converterPanel) {
+        // TODO Auto-generated constructor stub
+        this.panel = converterPanel;
+    }
 
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		// TODO Auto-generated method stub
-		//panel = new ConverterPanel();
-		
-		if (e.getActionCommand() != null) {
-			String text = getPanel().getCentimetersConversionArea().getTextArea().getText();
-			if ( text == null ) { // need to throw exception here: NumberFormatException
-				System.out.println("text is null");
-			}
-			else {
-			System.out.println(text);
-			double data = Double.parseDouble(text);
-			String meter = Double.toString(data/100);
-			String feet = Double.toString(data/30.48);
-			panel.getMeterConversionArea().SetText(meter+" m");
-			panel.getFeetConversionArea().SetText(feet+ " ft");
-			}
-		}
-		
-	}
-	
-	public ConverterPanel getPanel() {
-		return this.panel;
-	}
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        //panel = new ConverterPanel();
+
+//        if (e.getActionCommand() != null) {
+        String text = getPanel().getCentimetersConversionArea().getTextArea().getText();
+
+        try {
+            System.out.println(text);
+            double data = Double.parseDouble(text);
+            String meter = Double.toString(data / 100);
+            String feet = Double.toString(data / 30.48);
+            panel.getMeterConversionArea().SetText(meter + " m");
+            panel.getFeetConversionArea().SetText(feet + " ft");
+        } catch (NumberFormatException nfe) {
+            System.out.println(nfe);
+        }
+    }
+
+    public ConverterPanel getPanel() {
+        return this.panel;
+    }
 
 }
